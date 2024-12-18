9b02b88091a532e12b6ee2cdaf8255b27fdd5b2a
federicomolino <federicomolino450@gmail.com>
2024-12-18 08:07:15 +0100

    add: esercizi

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -15,18 +15,35 @@ const fourthTeacher = null;
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = null;
+const lastTeacher = teachers.pop();
+
+console.log(lastTeacher);
+console.log.apply(teachers);
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+const firstTeacher = teachers.shift();
+
+console.log(firstTeacher);
+console.log(teachers);
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
+teachers.push("Vanessa");
+console.log(teachers);
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
+teachers.unshift("Sarah");
+console.log(teachers);
 
 // 7. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+
+for (let index = 0; index <= teachers.length; index++) {
+  const isFabioPresent = teachers[index];
+  
+  if (isFabioPresent === 'Fabio') {
+    console.log(isFabioPresent);
+  }
+}
 
 // 8. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
